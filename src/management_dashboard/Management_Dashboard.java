715bcd77a8b87cea555ff27b4663da994a140c6c
user <user@DESKTOP-LKTQ0L2>
2022-08-12 13:42:42 +0100

    Management_Dashboard

---
+++ src/management_dashboard/Management_Dashboard.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package management_dashboard;
+
+/**
+ *
+ * @author user
+ */
+public class Management_Dashboard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("management_dashboard.Management_Dashboard.main()");
+        // TODO code application logic here
+    }
+    
+}
